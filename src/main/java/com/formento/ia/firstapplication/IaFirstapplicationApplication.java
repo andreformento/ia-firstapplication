af2de4957e66510f66566a554646c4d59858ddf4
Andre Formento <andreformento.sc@gmail.com>
2017-04-03 23:42:05 -0300

    First commit

---
+++ src/main/java/com/formento/ia/firstapplication/IaFirstapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.formento.ia.firstapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaFirstapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaFirstapplicationApplication.class, args);
+	}
+}
