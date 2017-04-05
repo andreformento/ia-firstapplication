7d466bda6668c3f3f9ee098dcc4b2bdef24b4fe9
Andre Formento <andreformento.sc@gmail.com>
2017-04-04 23:32:32 -0300

    add new method

--- src/main/java/com/formento/ia/firstapplication/model/Route.java
+++ src/main/java/com/formento/ia/firstapplication/model/Route.java
@@ -1,25 +1,31 @@
 package com.formento.ia.firstapplication.model;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Route {
 
-    private final Coordinate start;
-    private final Coordinate end;
+    private final Localization start;
+    private final Localization end;
 
-    public Route(final Coordinate start, final Coordinate end) {
+    public Route(final Localization start, final Localization end) {
         checkNotNull(start);
         checkNotNull(end);
         this.start = start;
         this.end = end;
     }
 
-    public Coordinate getStart() {
+    public Localization getStart() {
         return start;
     }
 
-    public Coordinate getEnd() {
+    public Localization getEnd() {
         return end;
+    }
+
+    public Iterable<Coordinate> getCoordinates() {
+        throw new NotImplementedException();
     }
 
 }
